9cb8e1e0122020c0e8287212d3215c9b1f7f38c4
justin-chen <justin.uob@gmail.com>
2016-12-30 09:37:09 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/nec/justin_chen/downloadimages/MainActivity.java
@@ -0,0 +1,75 @@
+package tw.com.nec.justin_chen.downloadimages;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView downloadedImage;
+
+
+    // 參數的意思是called by a view
+    public void downloadImage(View view){
+
+        //Log.i("Interaction","Button Tapped");
+        // https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png").get();
+            downloadedImage.setImageBitmap(myImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        downloadedImage = (ImageView) findViewById(R.id.imageView);
+
+
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // download the whole inputstream in one go
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                // convert data downloaded into an image
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+                return myBitmap;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+}
